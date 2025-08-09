a61e5079d485c2a767f8a33bc13a527cb0764b88
Ujjwal <Ujjwalrawat248@gmail.com>
2025-08-09 18:30:24 +0530

    [ADD:CountDownLatch]

--- src/main/java/org/example/executionarframework/SampleCountDownLatch.java
+++ src/main/java/org/example/executionarframework/SampleCountDownLatch.java
@@ -1,4 +1,52 @@
 package org.example.executionarframework;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class SampleCountDownLatch {
+    public static void main(String[] args) {
+
+        int noOfService=3;
+        CountDownLatch latch=new CountDownLatch(noOfService);
+        ExecutorService executorService= Executors.newFixedThreadPool(noOfService);
+
+        executorService.submit(new DService(latch));
+        executorService.submit(new DService(latch));
+        executorService.submit(new DService(latch));
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Running the main method");
+        executorService.shutdown();
+
+    }
+}
+
+
+class DService implements Callable<String> {
+
+    private CountDownLatch latch;
+
+    DService(CountDownLatch latch){
+        this.latch=latch;
+    }
+
+    @Override
+    public String call() throws Exception {
+        try {
+            System.out.println("wait for 2 sec....");
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }finally {
+            latch.countDown();
+        }
+        return "Calling the call method of the callable interface";
+    }
 }
